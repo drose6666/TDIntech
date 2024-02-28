3e06b5cb351e0502d1c167068c0c2cc3a459c2d5
Danila Rose <danila.rose.web@gmail.com>
2024-02-28 22:29:43 +0200

    html structure of the contact-screen

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,4 @@ import '../scss/index.scss';
 import './common.js';
 import '../components/screen/catalog-screen/catalog-screen.js';
 import '../components/screen/reviews-screen/reviews-screen.js';
+import '../components/forms/contact-form/contact-form.js';
